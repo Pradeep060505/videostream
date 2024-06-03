1d8f07daf04f59d3071c8c7c5429fc075ebc078c
Pradeep Rathod <104147377+Pradeep060505@users.noreply.github.com>
2024-06-03 05:54:08 +0530

    done with ffmpeg

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
 import path from "path";
 import fs from "fs";
-import {} from "child_process";
+import { exec } from "child_process";
+import { stderr, stdout } from "process";
 
 const app = express();
 
@@ -57,7 +58,23 @@ app.post("/upload", upload.single("file"), function (req, res) {
     fs.mkdirSync(outputPath, { recursive: true });
   }
 
+  //ffmpeg
+  const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/segment%03d.ts" -start_number 0 ${hlsPath}`;
 
+  exec(ffmpegCommand, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`exec error: ${error}`);
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    const videoUrl = `http://localhost:8000/uploads/courses/${lessonId}/index.m3u8`;
+
+    res.json({
+      message: "Video converted to HLS format",
+      videoUrl: videoUrl,
+      lessonId: lessonId,
+    });
+  });
 });
 
 app.listen(8000, function () {
